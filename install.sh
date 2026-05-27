0c443d00427b800251eca6b263d91383adbb9b45
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-05-27 12:31:56 -0300

    1.1

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "Instalando..."
+
+
+cat << "EOF"
+
+████  █████ ████  █████ █   █      ████  ████  
+█   █ █     █   █ █     █  █       █   █ █   █ 
+████  ████  █   █ ████  ███   ████ █   █ ████  
+█  █  █     █   █ █     █  █       █   █ █   █ 
+█   █ █████ ████  █████ █   █      ████  ████ 
+
+            ISTALLER REDEK-DB
+
+EOF
+
+echo ""
+echo "Construa a imagem Docker:"
+docker build -t ubuntuweb .
+echo ""
+echo "Execute o container:"
+docker run -d --name ubuntuweb -p 6080:6080 -p 5901:5901 ubuntuweb
+echo "localhost:6080/vnc.html"
+echo ""
