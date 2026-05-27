ce8f1afa8bf18a306d93b8ba7c7e8e0874445185
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-05-27 12:55:55 -0300

    1.1

--- install.sh
+++ install.sh
@@ -22,5 +22,6 @@ docker build -t ubuntuweb .
 echo ""
 echo "Execute o container:"
 docker run -d --name ubuntuweb -p 6080:6080 -p 5901:5901 ubuntuweb
+echo ""
 echo "localhost:6080/vnc.html"
 echo ""
