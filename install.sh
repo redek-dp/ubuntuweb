4dce0a372c9e8d725903909eb5dbf6d98fa2c6cc
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-06-27 16:36:24 -0300

    1.1

--- install.sh
+++ install.sh
@@ -21,7 +21,11 @@ echo "Construa a imagem Docker:"
 docker build -t ubuntu-vnc .
 echo ""
 echo "Execute o container:"
-docker run -d \ --name ubuntu-vnc \ -p 5901:5901 \ -p 6080:6080 \ ubuntu-vnc
+docker run -d \
+--name ubuntu-vnc \
+-p 5901:5901 \
+-p 6080:6080 \
+ubuntu-vnc
 echo ""
 echo "localhost:6080/vnc.html"
 echo ""
